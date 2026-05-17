a570b5790b34516faaa7a3066d17af1ea8ac2cfb
Riverxia <riverdalecoder@outlook.com>
2026-05-17 14:46:57 -0400

    (acm) Currently Listening To: UNION!! - 765 MILLION ALLSTARS

--- src/main/java/sh/idktheflag/idkhack/impl/IdkHackMod.java
+++ src/main/java/sh/idktheflag/idkhack/impl/IdkHackMod.java
@@ -24,7 +24,7 @@ public class IdkHackMod implements ModInitializer
     /**
      * Git hash of the client
      */
-    public static final String HASH = "b92da7855b414aa7";
+    public static final String HASH = "f491256debc2fd04";
 
     /**
      * The snowflake text ❄
